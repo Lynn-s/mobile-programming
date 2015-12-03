44f3002daab7cd3f52308137fe7a61b8cb655d1d
Lynn-s <penguin_shin@naver.com>
2015-12-03 19:32:48 +0900

    Update StackGeneric.java

--- src/lab_stack/StackGeneric.java
+++ src/lab_stack/StackGeneric.java
@@ -1,7 +1,10 @@
 package lab_stack;
 
+//lab_05 stack using generic
+//20143073 신예린
+
 public class StackGeneric {
-	   public static void main(String arg[]){
+	public static void main(String arg[]){
 	      Stack<Integer> stackInteger = new Stack<Integer>();   
 	      stackInteger.push(1);
 	      stackInteger.push(2);
@@ -13,44 +16,44 @@ public class StackGeneric {
 	      stackString.push("Korea Seoul");
 	      System.out.println(stackString.pop());
 	      System.out.println(stackString.pop());
-	   }
 	}
+}
 
-	class Stack<T> {
-	   Node<T> top;
+class Stack<T> {
+	Node<T> top;
 	   
-	   public T pop() {
-	      T data = top.getData();
-	      top = top.getNext();
-	      return data;
+	public T pop() {
+	   T data = top.getData();
+	   top = top.getNext();
+	   return data;
+	}
+	   
+	public void push(T data) {
+	   if(top == null){
+	      top = new Node<T>(data);
 	   }
-	   
-	   public void push(T data) {
-	      if(top == null){
-	         top = new Node<T>(data);
-	      }
-	      else{
-	         Node<T> nextNode = new Node<T>(data);
-	         nextNode.setNext(top);
-	         top = nextNode;
-	      }
+	   else{
+	      Node<T> nextNode = new Node<T>(data);
+	      nextNode.setNext(top);
+	      top = nextNode;
 	   }
 	}
+}
 
 class Node<T> {
-	   Node<T> next;
-	   T data;
-	   public Node(T data) {
-	       this.data = data;
-	    }
-	   public T getData() {
-	      return this.data;
-	   }
-	   public Node<T> getNext() {
-	      return this.next;
-	   }
-	   public void setNext(Node<T> next) {
-	      this.next = next;
-	   }
+	Node<T> next;
+	T data;
+	public Node(T data) {
+	    this.data = data;
 	}
+	public T getData() {
+	   return this.data;
+	}
+	public Node<T> getNext() {
+	   return this.next;
+	}
+	public void setNext(Node<T> next) {
+	   this.next = next;
+	}
+}
 	
